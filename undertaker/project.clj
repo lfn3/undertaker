a5da929ffc22ee2170607dd126f57039593fbe54
lfn3 <liam@lfn3.net>
2017-07-27 18:59:20 +1200

    Push down undertaker to a lower level.
    Going to create a junit project alongside and pull out the bits for that.

---
+++ undertaker/project.clj
@@ -0,0 +1,18 @@
+(defproject undertaker "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/spec.alpha "0.1.123"]
+                 [orchestra "0.3.0"]
+                 [org.clojure/test.check "0.9.0"]
+                 [criterium "0.4.4"]
+                 [junit/junit "4.12"]]
+
+  :source-paths ["src/main/clojure"]
+  :java-source-paths ["src/main/java"]
+  :test-paths ["src/test/clojure" "src/test/java"]
+  :target-path "target/%s"
+
+  :aot [undertaker.source-rule])
