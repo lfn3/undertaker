59affe99cd8239c676ec6687d9b4e0b9a135f562
lfn3 <liam@lfn3.net>
2017-08-02 08:14:03 +1200

    Split up tests into shrinking and core (i.e. generation)
    This is so that the shrinking tests don't run into the error I just added
    to catch double binding of the the *source* var.

--- undertaker/src/test/clojure/undertaker/core_test.clj
+++ undertaker/src/test/clojure/undertaker/core_test.clj
@@ -53,52 +53,6 @@
   (is (every? int? (undertaker/vec-of undertaker/int)))
   (is (not-empty (undertaker/vec-of undertaker/byte 1))))
 
-(deftest should-shrink-two-steps
-  (is (= [0] (vec (proto/get-sourced-bytes (undertaker/shrink (byte-array [2])
-                                                              []
-                                                              (constantly {::undertaker/result false})))))))
-
-(deftest should-not-shrink-to-zero-if-does-not-fail-on-zero-shrinker
-  (is (not (zero? (-> (undertaker/shrink (byte-array [2])
-                                         []
-                                         (undertaker/wrap-fn (fn [] {::undertaker/result (is (= 0 (undertaker/byte)))})))
-                      (proto/get-sourced-bytes)
-                      (first))))))
-
-(deftest should-shrink-past-1
-  (is (= [0] (-> (undertaker/shrink (byte-array [5])
-                                    []
-                                    (undertaker/wrap-fn (fn [] {::undertaker/result (is (= 1 (undertaker/byte)))})))
-                 (proto/get-sourced-bytes)
-                 (vec)))))
-
-(deftest should-shrink-to-2
-  (is (= [2] (-> (undertaker/shrink (byte-array [80])
-                                    []
-                                    (undertaker/wrap-fn (fn [] {::undertaker/result (let [value (undertaker/byte)]
-                                                                                      (is (if (= 0 value)
-                                                                                            true
-                                                                                            (odd? value))))})))
-                 (proto/get-sourced-bytes)
-                 (vec)))))
-
-(deftest can-run-prop
-  (is (true? (::undertaker/result (undertaker/run-prop {} (constantly true))))))
-
-(deftest should-shrink-to-zero
-  (is (= 0 (->> #(is (boolean? (undertaker/byte)))
-                (undertaker/run-prop {})
-                ::undertaker/shrunk-values
-                (first)))))
-
-(deftest should-not-shrink-to-zero-if-does-not-fail-on-zero-prop
-  (is (->> #(is (= 0 (undertaker/byte)))
-           (undertaker/run-prop {})
-           ::undertaker/shrunk-values
-           (first)
-           (zero?)
-           (not))))
-
 (deftest byte-gen-test
   (is (= 1 (undertaker/byte 1 1)))
   (let [results (repeatedly 10 #(undertaker/byte -1 0))]
@@ -142,9 +96,6 @@
   (let [val (undertaker/int -1 1)]
     (is (= (Integer/signum val) val))))
 
-(deftest can-fail-prop
-  (is (false? (::undertaker/result (undertaker/run-prop {} #(is false))))))
-
 (deftest next-byte-for-int-should-emit-only-number-in-range
   (let [values (repeatedly 10 #(undertaker/generate-next-byte-for-int forgetful-source
                                                                       0
@@ -164,50 +115,6 @@
 (deftest from-gen-test
   (let [values (set (repeatedly 3 undertaker/int))]
     (is (values (undertaker/from values)))))
-
-(deftest snip-interval-test
-  (is (= [0 0] (vec (undertaker/snip-interval (byte-array [0 1 1 0]) {::proto/interval-start 1
-                                                                      ::proto/interval-end   3})))))
-
-(deftest snip-intervals-should-handle-overrun-exceptions
-  (is (= [0] (-> (byte-array [0])
-                 (undertaker/snip-intervals [{::proto/interval-start 0
-                                              ::proto/interval-end   0}]
-                                            (undertaker/wrap-fn #(undertaker/int)))
-                 (vec)))))
-
-(deftest snip-intervals-handles-single-byte-failure
-  (is (= [-19] (-> (byte-array [1 -19])
-                   (undertaker/snip-intervals [{::proto/interval-start 0
-                                                ::proto/interval-end   1}]
-                                              (undertaker/wrap-fn #(is (boolean? (undertaker/byte)))))
-                   (vec)))))
-
-(deftest should-shrink-middle-byte
-  (let [result (->> #(let [bool-1 (undertaker/bool)
-                           a-number (undertaker/int)
-                           bool-2 (undertaker/bool)]
-                       (is (not bool-1)))
-                    (undertaker/run-prop {}))]
-    (is (= [true 0 false] (-> result
-                              ::undertaker/shrunk-values
-                              (vec)))
-        result)))
-
-(deftest should-shrink-vec-to-smallest-failing-case
-  (let [result (->> #(let [values (undertaker/vec-of undertaker/byte)]
-                       (is (every? even? values)))
-                    (undertaker/run-prop {}))
-        shrunk-vector (->> result
-                           ::undertaker/shrunk-values
-                           (first))]
-    (is (or (= [1] shrunk-vector)
-            (= [-1] shrunk-vector)) result)))
-
-(deftest should-only-run-once-since-source-is-unused
-  (let [result (->> (fn [])
-                    (undertaker/run-prop {}))]
-    (is (= 1 (::undertaker/iterations-run result)))))
 
 (deftest long-gen-test
   (is (instance? Long (undertaker/long)))
