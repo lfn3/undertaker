8150001eb71bba8aad527c875c67345cbccd0d76
lfn3 <liam@lfn3.net>
2017-09-09 16:48:45 +1200

    Upgrade clojure to alpha-20

--- undertaker-junit/project.clj
+++ undertaker-junit/project.clj
@@ -5,7 +5,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  [org.clojure/spec.alpha "0.1.123"]
                  [orchestra "0.3.0"]
                  [org.clojure/test.check "0.9.0"]
--- undertaker/project.clj
+++ undertaker/project.clj
@@ -5,7 +5,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  [org.clojure/spec.alpha "0.1.123"]]
 
   :source-paths ["src/main/clojure"]
