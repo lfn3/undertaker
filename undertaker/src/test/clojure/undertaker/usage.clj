06b2320a35aedc43fe9b63966b91274d39d3bd08
lfn3 <liam@lfn3.net>
2017-09-10 11:41:15 +1200

    Ascii character generator

--- undertaker/src/test/clojure/undertaker/usage.clj
+++ undertaker/src/test/clojure/undertaker/usage.clj
@@ -67,3 +67,12 @@
   (let [string (undertaker/string 1 1)]
     (is (instance? String string))
     (is (= 1 (count string)))))
+
+(def ascii-chars #{\space \@ \` \! \A \a \" \B \b \# \C \c \$ \D \d \% \E \e \& \F \f \' \G \g \( \H \h \) \I \i \* \J
+                   \j \+ \K \k \, \L \l \- \M \m \. \N \n \/ \O \o \0 \P \p \1 \Q \q \2 \R \r \3 \S \s \4 \T \t \5 \U
+                   \u \6 \V \v \7 \W \w \8 \X \x \9 \Y \y \: \Z \z \; \[ \{ \< \\ \| \= \] \} \> \^ \~ \? \_})
+
+(undertaker/defprop can-get-ascii-chars {}
+  (let [c (undertaker/char-ascii)]
+    (is (instance? Character c))
+    (is (ascii-chars c))))
