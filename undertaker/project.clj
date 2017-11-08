54555b2f2694a61b3ace31d56b6d2d02693e73ff
lfn3 <liam@lfn3.net>
2017-11-09 12:16:22 +1300

    Add a jmh benchmark using lein-jmh
    At the moment it isn't working. Hopefully the
    (issue I've submitted)[https://github.com/jgpc42/lein-jmh/issues/4]
    will get some traction and then I can commit this.
    
    Going to continue with criterium in the interim.

--- undertaker/project.clj
+++ undertaker/project.clj
@@ -8,14 +8,17 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
                  [org.clojure/spec.alpha "0.1.123"]]
 
+  :plugins [[lein-jmh "0.2.2"]]
+
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
   :test-paths ["src/test/clojure"]
   :target-path "target/%s"
 
   :profiles {:dev {:dependencies [[orchestra "0.3.0"]
-                                   [org.clojure/test.check "0.9.0"]
-                                   [criterium "0.4.4"]]}
+                                  [org.clojure/test.check "0.9.0"]
+                                  [criterium "0.4.4"]]}
+             :jmh {:jvm-opts []}
              :benchmarks {:test-paths ^:replace ["src/test/benchmarks"]
                           :dependencies [[criterium "0.4.4"]
                                          [org.clojure/test.check "0.9.0"]]}}
