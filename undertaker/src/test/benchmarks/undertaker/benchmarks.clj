d91b445ddcf7d83ee35e26509505af2e4222d0fe
lfn3 <liam@lfn3.net>
2017-11-10 19:00:49 +1300

    Add another benchmark and it's result.

--- undertaker/src/test/benchmarks/undertaker/benchmarks.clj
+++ undertaker/src/test/benchmarks/undertaker/benchmarks.clj
@@ -12,3 +12,10 @@
 (defn kv-gen [] (fn [] [(undertaker.core/keyword) (undertaker.core/keyword)]))
 
 (defn string-kv-gen [] (fn [] [(undertaker.core/string) (undertaker.core/string)]))
+
+(defn java-api-get-string [size-of-vector]
+  (let [codepoint-gen (partial undertaker.core/int 48 57 65 90 97 122)]
+    (->> (undertaker.core/vec-of codepoint-gen size-of-vector size-of-vector)
+         (map char)
+         (char-array)
+         (String.))))
