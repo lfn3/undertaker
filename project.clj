0ff269346706b61f96ca7774dfc3e35d85950f68
lfn3 <liam@lfn3.net>
2017-11-20 08:46:08 +1300

    Clean up deps.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
-                 [org.clojure/spec.alpha "0.1.123"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
                  [com.lmax/undertaker ~version]
                  [com.lmax/undertaker-junit ~version]])
--- undertaker-junit/project.clj
+++ undertaker-junit/project.clj
@@ -5,9 +5,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha20"]
-                 [org.clojure/spec.alpha "0.1.123"]
-                 [orchestra "0.3.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [orchestra "2017.11.12-1"]
                  [org.clojure/test.check "0.9.0"]
                  [criterium "0.4.4"]
                  [junit/junit "4.12"]
