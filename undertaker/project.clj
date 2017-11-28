8ca5a7cfc5fcce90f669199da05c3e1ad48cd6ac
Liam Falconer <falconerl@lmax.com>
2017-11-28 16:54:40 +1300

    Flatten junit stuff into main project.

--- undertaker/project.clj
+++ undertaker/project.clj
@@ -7,23 +7,33 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies []
 
-  :plugins [[lein-jmh "0.2.2"]]
+  :plugins [[lein-jmh "0.2.2"]
+            [lein-junit "1.1.8"]]
 
   :source-paths ["src/main/clojure"]
   :java-source-paths ["src/main/java"]
-  :test-paths ["src/test/clojure" "src/test/benchmarks"]
+  :test-paths ["src/test/clojure" "src/test/java" "src/test/benchmarks"]
   :target-path "target/%s"
 
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.9.0-RC1"]]}
-             :dev      {:source-paths ["src/specs/clojure"]
-                        :jvm-opts     ["-Dundertaker.debug=true"]
-                        :dependencies [[orchestra "2017.11.12-1"]
-                                       [org.clojure/test.check "0.9.0"]]}
+  :profiles {:provided  {:dependencies [[org.clojure/clojure "1.9.0-RC1"]]}
+             :dev       {:source-paths      ["src/specs/clojure"
+                                             "src/junit/clojure"]
+                         :java-source-paths ["src/junit/java"]
+                         :jvm-opts          ["-Dundertaker.debug=true"]
+                         :dependencies      [[junit "4.12"]
+                                             [orchestra "2017.11.12-1"]
+                                             [org.clojure/test.check "0.9.0"]]
+                         :aot [net.lfn3.undertaker.junit.source-rule]}
 
-             :specs-jar {:source-paths ^:replace ["src/specs/clojure"]
+             :specs-jar {:source-paths      ^:replace ["src/specs/clojure"]
                          :java-source-paths ^:replace []
-                         :jar-name "undertaker-specs.jar"}
+                         :jar-name          "undertaker-specs.jar"}
 
-             :test     {:source-paths ["src/specs/clojure"]
-                        :jvm-opts ["-Dundertaker.debug=true" "-Xmx8g"]}
-             :jmh      {:jvm-opts ["-Dundertaker.debug=false"]}})
+             :junit-jar {:dependencies      [[junit "4.12"]]
+                         :source-paths      ^:replace ["src/junit/clojure"]
+                         :java-source-paths ^:replace ["src/junit/java"]
+                         :aot [net.lfn3.undertaker.junit.source-rule]}
+
+             :test      {:source-paths ["src/specs/clojure"]
+                         :jvm-opts     ["-Dundertaker.debug=true" "-Xmx8g"]}
+             :jmh       {:jvm-opts ["-Dundertaker.debug=false"]}})
