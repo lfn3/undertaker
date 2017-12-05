aa97d56da4ea78aa67ec262f2d31d0f209e0138b
Liam Falconer <falconerl@lmax.com>
2017-12-05 16:10:07 +1300

    Snip-interval returns a byte array, which won't ever be nil.

--- src/main/clojure/net/lfn3/undertaker/shrink.clj
+++ src/main/clojure/net/lfn3/undertaker/shrink.clj
@@ -54,23 +54,21 @@
     (if (not-empty intervals)
       (let [interval (nth intervals index)
             continue? (< (inc index) (count intervals))]
-        (if-let [shrunk-bytes (snip-interval bytes interval)]
-          (let [source (fixed-source/make-fixed-source shrunk-bytes)
-                result (fn source)
-                passed? (:net.lfn3.undertaker.core/result result)
-                overrun? (is-overrun? result)]
-            (cond
-              (and continue? (or overrun? passed?)) (recur (inc index)
-                                                           intervals
-                                                           bytes)
-              ;TODO: figure out if I can optimize this a bit.
-              (and continue? (not passed?) (not overrun?)) (recur 0 ;safest option is to restart, since we might have deleted a bunch of intervals.
-                                                                  (filter is-snippable? (source/get-intervals source))
-                                                                  shrunk-bytes)
-              (and (not continue?) (or overrun? passed?)) bytes
-              (and (not continue?) (not overrun?) (not passed?)) shrunk-bytes))
-          (when continue?
-            (recur (inc index) intervals bytes))))
+        (let [shrunk-bytes (snip-interval bytes interval)
+              source (fixed-source/make-fixed-source shrunk-bytes)
+              result (fn source)
+              passed? (:net.lfn3.undertaker.core/result result)
+              overrun? (is-overrun? result)]
+          (cond
+            (and continue? (or overrun? passed?)) (recur (inc index)
+                                                         intervals
+                                                         bytes)
+            ;TODO: figure out if I can optimize this a bit.
+            (and continue? (not passed?) (not overrun?)) (recur 0 ;safest option is to restart, since we might have deleted a bunch of intervals.
+                                                                (filter is-snippable? (source/get-intervals source))
+                                                                shrunk-bytes)
+            (and (not continue?) (or overrun? passed?)) bytes
+            (and (not continue?) (not overrun?) (not passed?)) shrunk-bytes)))
       bytes)))
 
 (defn shrink-at!
