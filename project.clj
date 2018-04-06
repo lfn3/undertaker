482e097abc4a2afb8eeb78a79bdd411d42bbae2a
Liam Falconer <falconerl@lmax.com>
2018-04-06 15:04:31 +1200

    Cut 0.1.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject net.lfn3/undertaker "0.1.4-SNAPSHOT"
+(defproject net.lfn3/undertaker "0.1.4"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Apache License Version 2.0"
